743573578c7c39106d395fb859a9aa9f9ad7f5ad
usdt.dev <y07710070321@gmail.com>
2026-04-24 22:01:42 +0100

    add setup logging with rotation

--- logger.py
+++ logger.py
@@ -1,42 +1,22 @@
 import logging
+from logging.handlers import RotatingFileHandler
 
-class Logger:
-    """Custom logger for the application."""
+# Configure logger settings
+LOG_FORMAT = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+LOG_FILE = 'roblox_tool.log'
+LOG_MAX_SIZE = 5 * 1024 * 1024  # 5 MB
+LOG_BACKUP_COUNT = 3
 
-    def __init__(self, name='roblox-tools'):
-        self.logger = logging.getLogger(name)
-        self.logger.setLevel(logging.DEBUG)
-        # Create console handler and set level
-        ch = logging.StreamHandler()
-        ch.setLevel(logging.DEBUG)
-        # Create formatter
-        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-        ch.setFormatter(formatter)
-        # Add the handlers to the logger
-        self.logger.addHandler(ch)
+# Initialize logger
+logger = logging.getLogger('RobloxTool')
+logger.setLevel(logging.DEBUG)
 
-    def debug(self, message):
-        """Log a debug message."""
-        self.logger.debug(message)
+# Create a rotating file handler
+handler = RotatingFileHandler(LOG_FILE, maxBytes=LOG_MAX_SIZE, backupCount=LOG_BACKUP_COUNT)
+handler.setFormatter(logging.Formatter(LOG_FORMAT))
 
-    def info(self, message):
-        """Log an info message."""
-        self.logger.info(message)
+# Add handler to logger
+logger.addHandler(handler)
 
-    def warning(self, message):
-        """Log a warning message."""
-        self.logger.warning(message)
-
-    def error(self, message):
-        """Log an error message."""
-        self.logger.error(message)
-
-    def critical(self, message):
-        """Log a critical message."""
-        self.logger.critical(message)
-
-# Example usage
 if __name__ == '__main__':
-    log = Logger()
-    log.info('This is an info message.')
-    log.error('This is an error message.')
+    logger.info('Logger setup complete')
